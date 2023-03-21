3781c9b6a83afd2d6313752aea082c50ec840db7
Langlois, Vince Thomas <vtlanglo@iu.edu>
2023-03-21 13:59:01 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# I311-MedicineDispenser
+
+An pill and water dispenser made for INFO-I341 *Prototyping with Arduino Tools* taught at IU Bloomington in Spring 2023
+
+Built using an Arduino UNO, a water pump, a servo motor, and ultrasonic sensors.
---
+++ waterPump/waterPump.ino
@@ -0,0 +1,47 @@
+/*
+  Ultrasonic Sensor HC-SR04 and Arduino Tutorial
+
+  by Dejan Nedelkovski,
+  www.HowToMechatronics.com
+
+*/
+// defines pins numbers
+const int trigPin = 10;
+const int echoPin = 9;
+const int motorPin = 5;
+// defines variables
+long duration;
+int distance;
+bool canDispense= true;
+
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  pinMode(motorPin, OUTPUT);
+  digitalWrite(motorPin, LOW);
+  Serial.begin(9600); // Starts the serial communication
+}
+void loop() {
+  // Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;
+  //Prints the distance on the Serial Monitor
+  if(distance == 2 && canDispense) {
+    canDispense = false;
+    digitalWrite(motorPin, HIGH);
+    delay(1000);
+  } else if (distance >= 10) {
+    digitalWrite(motorPin, LOW);
+    canDispense = true;
+  } else if (canDispense == false) {
+    digitalWrite(motorPin, LOW);
+  }
+}
